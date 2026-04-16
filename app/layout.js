9ed4615608bdb367fc856b9730db755c22523432
Syed-Faizann <syedfaizanali@gmail.com>
2026-04-16 09:44:36 +0500

    implement navigation

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,8 +21,9 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
+      <Header/>
       <body className="min-h-full flex flex-col ">{children}</body>
     </html>
   );
